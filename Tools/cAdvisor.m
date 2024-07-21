6519445a31101fc0befd8517e1b64ab498f80995
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-07-21 13:25:40 +0530

    Create cAdvisor.m

---
+++ Tools/cAdvisor.m
@@ -0,0 +1,11 @@
+# cAdvisor Download command
+
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  gcr.io/cadvisor/cadvisor:v0.39.3
