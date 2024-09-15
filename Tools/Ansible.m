1a7e3088d28ffa39292a11adb18a9af966bceef6
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-09-15 09:52:13 +0530

    Update Ansible.m

--- Tools/Ansible.m
+++ Tools/Ansible.m
@@ -7,15 +7,4 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 sudo apt install git -y 
-mkdir Ansible && cd Ansible
-vi synk.yml
-- name: Install DevOps Tools
-  hosts: localhost
-  become: yes
-  tasks:
-    - name: Download and install Snyk
-      ansible.builtin.get_url:
-        url: https://static.snyk.io/cli/latest/snyk-linux
-        dest: /usr/local/bin/snyk
-        mode: '0755'
-ansible-playbook -i localhost synk.yml
+
