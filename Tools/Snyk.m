f090d6c543b60f7e6d7c61f590ecb2772058ae8d
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-09-15 09:51:51 +0530

    Create Snyk.m

---
+++ Tools/Snyk.m
@@ -0,0 +1,22 @@
+# Synk Installation using Ansible Playbook
+
+Install Anisble Firstly
+Make Directory Ansible
+Then create a "yaml" file ansible-playbook in the above directory
+
+vi synk.yaml
+
+---
+- name: Install DevOps Security Tool
+  hosts: localhost
+  become: yes
+  tasks:
+    - name: Download and install Snyk
+      ansible.builtin.get_url:
+        url: https://static.snyk.io/cli/latest/snyk-linux
+        dest: /usr/local/bin/snyk
+        mode: '0755'
+---
+
+after saving the above "yaml" file run the command for Intallation of Synk: 
+ansible-playbook -i localhost synk.yml
