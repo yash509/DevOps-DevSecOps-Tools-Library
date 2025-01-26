96fa7bf123bd28d25c49d508c8cdd3ed88037789
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-01-26 07:03:02 +0530

    Update InfluxDB.m

--- Tools/InfluxDB.m
+++ Tools/InfluxDB.m
@@ -1,1 +1,14 @@
 docker run -d -p 8086:8086 --name influxdb2 influxdb:1.8.6-alpine
+
+
+
+
+docker exec -it influxdb2 bash
+influx
+CREATE DATABASE "jenkins" WITH DURATION 1825d REPLICATION 1 NAME "jenkins-retention"
+SHOW DATABASES
+USE database_name
+
+
+
+
