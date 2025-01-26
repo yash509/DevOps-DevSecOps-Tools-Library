0fa46d9e84414dec96d835a752a3e28ca548e1fe
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-01-26 11:51:55 +0530

    Update owasp-zap.m

--- Tools/owasp-zap.m
+++ Tools/owasp-zap.m
@@ -12,3 +12,7 @@ stage('OWASP ZAP Penetration Testing - DAST'){
                 sh "docker run -v \$(pwd):/zap/wrk/:rw --network=\"host\" zaproxy/zap-stable zap-baseline.py -t http://15.206.125.79:5000 || true"
             }
         }
+
+
+
+https://github.com/devsecblueprint/dast-scanning-with-zap <--- Reference
