6a57651d10d7f866a2cd98dfe6c826fc4904cc1d
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-01-30 09:35:31 +0530

    Update Docker-Scout.m

--- Tools/Docker-Scout.m
+++ Tools/Docker-Scout.m
@@ -6,3 +6,13 @@
 docker login
 
 curl -sSfL https://raw.githubusercontent.com/docker/scout-cli/main/install.sh | sh -s -- -b /usr/local/bin
+
+sudo mkdir -p /tmp/docker-scout/sha256
+
+sudo chmod -R 777 /tmp/docker-scout
+
+sudo usermod -aG docker jenkins
+
+sudo chown -R jenkins:jenkins /var/lib/jenkins/workspace
+
+sudo chmod -R 775 /var/lib/jenkins/workspace
